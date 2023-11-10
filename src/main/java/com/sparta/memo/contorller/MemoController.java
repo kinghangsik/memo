c9d5e5d0e5e005dc15c283ed9a0d00c349514a68
kinghangsik <hgt9107@naver.com>
2023-11-10 13:38:32 +0900

    update, delete 구현

--- src/main/java/com/sparta/memo/contorller/MemoController.java
+++ src/main/java/com/sparta/memo/contorller/MemoController.java
@@ -15,6 +15,7 @@ import java.util.Map;
 public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
+    //create 기능
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
         // RequestDto -> entity
@@ -32,7 +33,7 @@ public class MemoController {
 
         return memoResponseDto;
     }
-
+    //read 기능
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
         //Map to List
@@ -42,4 +43,35 @@ public class MemoController {
 
         return responseList;
     }
+
+    //update 기능
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        //해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)) {
+            //해당 메모 가져오기
+            Memo memo = memoList.get(id);
+
+            //메모 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    //delete 기능
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        //해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)) {
+            //해당 메모 삭제하기
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new RuntimeException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+
 }
